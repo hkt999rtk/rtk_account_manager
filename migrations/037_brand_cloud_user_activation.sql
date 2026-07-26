5d920d7f4af416b0e5f7a3c0b96a1125682b010e
Kevin Huang <hkt999@gmail.com>
2026-07-26 22:48:33 +0900

    Allow brand owner activation email outbox

--- migrations/037_brand_cloud_user_activation.sql
+++ migrations/037_brand_cloud_user_activation.sql
@@ -9,3 +9,18 @@ ALTER TABLE auth_tokens
         'login_activation',
         'brand_cloud_user_activation'
     ));
+
+ALTER TABLE email_outbox
+    DROP CONSTRAINT IF EXISTS email_outbox_message_type_check;
+
+ALTER TABLE email_outbox
+    ADD CONSTRAINT email_outbox_message_type_check
+    CHECK (message_type IN (
+        'email_verification',
+        'login_activation',
+        'password_reset',
+        'brand_cloud_owner_transfer',
+        'brand_cloud_user_activation',
+        'quota_approved',
+        'quota_declined'
+    ));
